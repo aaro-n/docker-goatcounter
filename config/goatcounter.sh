9c992bb8b3078e8578fb77e01d4c79a0ed2d1a8b
Aaron <qcqilin@outlook.com>
2024-08-09 18:15:38 +0800

    12

--- config/goatcounter.sh
+++ config/goatcounter.sh
@@ -3,8 +3,8 @@
 # 启动 GoatCounter 并记录日志
 exec 2>&1
 
-# 初始化参数数组
-GOATCOUNTER_COMMAND=()
+# 初始化命令字符串
+GOATCOUNTER_COMMAND=""
 
 # 循环处理每个 GOATCOUNTER_COMMAND_n
 i=1
@@ -15,13 +15,13 @@ while true; do
     # 如果没有找到命令，退出循环
     [ -z "$CMD" ] && break
     
-    # 将当前命令添加到数组中
-    GOATCOUNTER_COMMAND+=("$CMD")
+    # 将当前命令添加到字符串中
+    GOATCOUNTER_COMMAND="$GOATCOUNTER_COMMAND $CMD"
     
     # 递增计数器
     i=$((i + 1))
 done
 
 # 启动 GoatCounter
-exec /home/goatcounter/bin/goatcounter serve -listen "$GOATCOUNTER_LISTEN" -automigrate -tls none -db "$GOATCOUNTER_DB" "${GOATCOUNTER_COMMAND[@]}"
+exec /home/goatcounter/bin/goatcounter serve -listen "$GOATCOUNTER_LISTEN" -automigrate -tls none -db "$GOATCOUNTER_DB" $GOATCOUNTER_COMMAND
 
