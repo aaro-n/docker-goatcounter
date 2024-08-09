0b8c8ebc5576762fae5caabe26cd0ba9f07ee3f1
Aaron <qcqilin@outlook.com>
2024-08-09 15:57:07 +0800

    追加变量GOATCOUNTER_COMMAND

--- config/goatcounter.sh
+++ config/goatcounter.sh
@@ -2,4 +2,15 @@
 
 # 启动 GoatCounter 并记录日志
 exec 2>&1
-exec /home/goatcounter/bin/goatcounter serve -listen "$GOATCOUNTER_LISTEN" -automigrate -tls none -db "$GOATCOUNTER_DB"
+
+# 基本命令
+command="/home/goatcounter/bin/goatcounter serve -listen \"$GOATCOUNTER_LISTEN\" -automigrate -tls none -db \"$GOATCOUNTER_DB\""
+
+# 检查 GOATCOUNTER_COMMAND 是否有内容
+if [ -n "$GOATCOUNTER_COMMAND" ]; then
+    command="$command $GOATCOUNTER_COMMAND"
+fi
+
+# 执行命令
+exec $command
+
